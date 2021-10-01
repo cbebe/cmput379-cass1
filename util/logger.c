8e6eac9d43e61a8c00e37e0a88617d03d5e1c718
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-10-01 00:35:33 -0600

    remove dev files

--- util/logger.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sys/types.h>  // ssize_t
-
-int main() {
-  char *buf = NULL;
-  size_t len = 0;
-  ssize_t lineSize = getline(&buf, &len, stdin);
-  printf("log: %s, lineSize = %ld", buf, lineSize);
-}
